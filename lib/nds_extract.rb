5bd4e213527c945cf013987d6f0776b957cc5c6e
Valerie Foster <fosterv2@outlook.com>
2020-01-14 01:29:15 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -34,53 +34,68 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
+  # GOAL: For each Hash in an Array (movies_collection), provide a collection of movies and a directors name to the movie_with_director_name
+  # method and accumulate the returned Array of movies into a new Array that's returned by this method.
   #
   # INPUT:
   # * name: A director's name
   # * movies_collection: An Array of Hashes where each Hash represents a movie
   #
   # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  # Array of Hashes where each Hash represents a movie; however, they should all have a :director_name key. This addition can be done
+  # by using the provided movie_with_director_name method
+  movie_array = []
+  movie_index = 0
+  while movie_index < movies_collection.length do
+    movie_array << movie_with_director_name(name, movies_collection[movie_index])
+    movie_index += 1
+  end
+  movie_array
 end
 
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
+  # return a Hash that includes the total worldwide_gross of all the movies from each studio.
   #
   # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
+  # * collection: Array of Hashes where each Hash represents a movie
   #
   # RETURN:
-  #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  studio_hash = {}
+  movie_index = 0
+  while movie_index < collection.length do
+    if !studio_hash[collection[movie_index][:studio]]
+      studio_hash[collection[movie_index][:studio]] = collection[movie_index][:worldwide_gross]
+    else
+      studio_hash[collection[movie_index][:studio]] += collection[movie_index][:worldwide_gross]
+    end
+    movie_index += 1
+  end
+  studio_hash
 end
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
+  # * source: An Array of Hashes containing director information including :name and :movies
   #
   # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  # Array of Arrays containing all of a director's movies. Each movie will need to have a :director_name key added to it.
+  movie_array = []
+  director_index = 0
+  while director_index < source.length do
+    movie_array << movies_with_director_key(source[director_index][:name], source[director_index][:movies])
+    director_index += 1
+  end
+  movie_array
 end
 
 # ----------------    End of Your Code Region --------------------
-# Don't edit the following code! Make the methods above work with this method
-# call code. You'll have to "see-saw" to get this to work!
+# Don't edit the following code! Make the methods above work with this method call code. You'll have to "see-saw" to get this to work!
 
 def studios_totals(nds)
   a_o_a_movies_with_director_names = movies_with_directors_set(nds)
